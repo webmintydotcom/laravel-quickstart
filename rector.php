eb2c9da8500fc5929751703ece6c664d0729ee6c
Webminty <git@webminty.com>
2025-10-14 13:04:50 -0700

    Swap to laravel rector

--- rector.php
+++ rector.php
@@ -4,24 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__.'/app',
-        __DIR__.'/bootstrap/app.php',
-        __DIR__.'/database',
-        __DIR__.'/public',
-        __DIR__.'/tests',
-    ])
     ->withSkip([
         AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    )
-    ->withPhpSets();
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true, /** other options */);
